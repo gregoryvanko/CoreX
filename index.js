a4eb7fe44d72da3ef4b605077b04407aa67891e2
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2020-03-21 14:26:57 +0100

    test

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ class corex {
         this._http = require('http').Server(this._Express)
     }
     
-
     set Debug(val){this._Debug = val}
     set AppIsSecured(val){this._AppIsSecured = val}
     set CSS(val){this._CSS = val}
