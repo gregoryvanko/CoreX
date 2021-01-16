d0f8fd1eea494255cbbcd05d276f7ad28ba09d53
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2021-01-16 14:52:29 +0100

    Sign Up

--- Client_CoreX_Login.js
+++ Client_CoreX_Login.js
@@ -117,20 +117,120 @@ class CoreXLogin {
         ConteneurForm.appendChild(Titre)
         Titre.setAttribute("id", "LoginTitre")
         Titre.innerText = document.title
+        // space
+        let Space = document.createElement("div")
+        ConteneurForm.appendChild(Space)
+        Space.setAttribute("style", "height: 3vh;")
         // SignUpBox
         let SignUpBox = document.createElement("div")
         ConteneurForm.appendChild(SignUpBox)
-        SignUpBox.setAttribute("id", "LoginBox")
-        // space
-        let Space = document.createElement("div")
-        SignUpBox.appendChild(Space)
-        Space.setAttribute("style", "height: 5vh;")
+        SignUpBox.setAttribute("class", "LoginBox")
         // Sous titre Sign Up
         let SousTitre = document.createElement("div")
         SignUpBox.appendChild(SousTitre)
         SousTitre.setAttribute("class", "LoginTextBig")
-        SousTitre.setAttribute("style", "text-align:left;")
         SousTitre.innerText = "Sign Up"
+        // space
+        let Space1 = document.createElement("div")
+        SignUpBox.appendChild(Space1)
+        Space1.setAttribute("style", "height: 3vh;")
+        // Email
+        SignUpBox.appendChild(this.BuildUserDataview("Email", "text", "100%"))
+        // Frist Name
+        SignUpBox.appendChild(this.BuildUserDataview("First-Name", "text", "100%"))
+        // Last Name
+        SignUpBox.appendChild(this.BuildUserDataview("Last-Name", "text", "100%"))
+        // PAssword
+        SignUpBox.appendChild(this.BuildUserDataview("Password", "password", "100%"))
+        // Error
+        let Error = document.createElement("div")
+        SignUpBox.appendChild(Error)
+        Error.setAttribute("id", "AccountErrorMsg")
+        Error.setAttribute("class", "LoginError")
+        Error.style.textAlign = "center"
+        Error.innerText = " "
+        // space
+        let Space2 = document.createElement("div")
+        SignUpBox.appendChild(Space2)
+        Space2.setAttribute("style", "height: 3vh;")
+        // Button
+        let divButton = document.createElement("div")
+        SignUpBox.appendChild(divButton)
+        divButton.setAttribute("style", "width:100%; text-align: center;")
+        let ButtonCreate = document.createElement("button")
+        divButton.appendChild(ButtonCreate)
+        ButtonCreate.setAttribute("Class", "LoginButton")
+        ButtonCreate.setAttribute("id", "AccountButton")
+        ButtonCreate.style.marginLeft = "auto"
+        ButtonCreate.style.marginRight = "auto"
+        ButtonCreate.innerText = "Create an Account"
+        ButtonCreate.onclick = this.CreateAccount.bind(this)
+    }
+
+    /** Construcuteur d'un element html pour une Key et une valeur */
+    BuildUserDataview(Key, Type, width){
+        let element = document.createElement("div")
+        element.setAttribute("style", "width: 100%; margin-top: 1vh;")
+        // Texte
+        let Text = document.createElement("div")
+        element.appendChild(Text)
+        Text.setAttribute("Class", "LoginText")
+        Text.setAttribute("Style", "width: " + width +";")
+        Text.innerText = Key.replace("-", " ")
+
+        let inputStyle="box-sizing: border-box; outline: none; margin: 0; background: white; -webkit-box-shadow: inset 0 1px 3px 0 rgba(0,0,0,.08); -moz-box-shadow: inset 0 1px 3px 0 rgba(0,0,0,.08); box-shadow: inset 0 1px 3px 0 rgba(0,0,0,.08); -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; color: #666; margin-bottom: 1vw; border: solid 1px rgba(0,0,0,.1); padding: 2%;"
+        let InputData = document.createElement("input")
+        element.appendChild(InputData)
+        InputData.setAttribute("id", Key)
+        InputData.setAttribute("Class", "LoginText CoreXWindowUserConfigInput")
+        InputData.setAttribute("Style", inputStyle + "width: " + width +";")
+        //InputData.setAttribute("value", Value)
+        InputData.setAttribute("type", Type)
+        InputData.setAttribute("name", Key)
+        //InputData.setAttribute("placeholder", Key.replace("-", " "))
+        return element
+    }
+
+    CreateAccount(){
+        if (this.InputUserAccountDataValide()){
+            let me = this
+            document.getElementById('AccountButton').innerHTML = "Waiting..."
+            var xhttp = new XMLHttpRequest()
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    let reponse = JSON.parse(this.responseText)
+                    if (reponse.Error) {
+                        console.log('AccountError : ' + reponse.ErrorMsg)
+                        document.getElementById("AccountErrorMsg").innerText=reponse.ErrorMsg
+                        document.getElementById('AccountButton').innerText = "Create an Account"
+                    } else {
+                        console.log('Create Account OK, Set Token')
+                        if(me._CallBackLogedIn != null){
+                            me._CallBackLogedIn(reponse.Token)
+                        } else {
+                            document.body.innerHTML = "Error : no CallBack fonction define after loged In"
+                        }
+                    }
+                } else if (this.readyState == 4 && this.status != 200){
+                    document.getElementById('AccountButton').innerText = "Create an Account"
+                    document.getElementById("AccountErrorMsg").innerText = this.response;
+                }
+            }
+            xhttp.open("POST", "CreateAccount", true)
+            xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+            xhttp.send(JSON.stringify({Email:document.getElementById('Email').value, FirstName:document.getElementById('First-Name').value, LastName:document.getElementById('Last-Name').value, Password:document.getElementById('Password').value}))
+        }
+    }
+
+    InputUserAccountDataValide(){
+        document.getElementById("AccountErrorMsg").innerHTML =" "
+        let ErrorMessage = ""
+        let IsValide = true
+        if (document.getElementById('LoginLoginValue').value.length < 1){
+            ErrorMessage += "Enter Login ";
+            IsValide = false;
+        }
+        return IsValide
     }
 
     /* Le template du screen login */
@@ -138,11 +238,12 @@ class CoreXLogin {
         let reponse = `
         <div style="display: flex; flex-direction: column; justify-content:space-between; align-content:center; align-items: center;">
             <div id="LoginTitre">` + Titre + /*html*/`</div>
-            <div id="LoginBox">
-                <div style="height: 5vh;"></div>
+            <div style="height: 5vh;"></div>
+            <div class="LoginBox" style="text-align: center;">
                 <input id="LoginLoginValue" class="LoginInput" type="text" name="LoginLoginValue" placeholder="Login" autofocus tabindex="1"> <br>
                 <input id="LoginPswValue" class="LoginInput" type="password" name="LoginPswValue" placeholder="Password" tabindex="2"> <br>
-			    <div id="LoginErrorMsg" class="LoginError"></div>
+                <div style="height: 4vh;"></div>
+                <div id="LoginErrorMsg" class="LoginError"></div>
                 <button id="LoginButtonLogin" class="LoginButton" tabindex="3">Send</button>
             </div>
             `
@@ -155,19 +256,6 @@ class CoreXLogin {
                 `
             }
         }
-        
-        // let LocalStorageApp = localStorage.getItem("CoreXApp")
-        // if(LocalStorageApp == "App"){
-        //     reponse += `
-        //     <div style="height:2vh;"></div>
-        //     <a href="/" id="GoToAdminApp" class="LoginText NoVisited">Go to Admin application</a>
-        //     </div>`
-        // } else {
-        //     reponse += `
-        //     <div style="height:2vh;"></div>
-        //     <a href="/" id="GoToAdminApp" class="LoginText NoVisited">Go to application</a>
-        //     </div>`
-        // }
         return reponse
     }
 
@@ -183,14 +271,6 @@ class CoreXLogin {
             }
             .LoginText{font-size: var(--CoreX-font-size);}
             .LoginTextBig{font-size: calc(var(--CoreX-font-size)*1.5);}
-
-            /*Box du login*/
-            #LoginBox{
-                width: 40%;
-                text-align: center;
-                margin: 0px auto 0px auto;
-                background-color: white;
-            }
 
             /*Message d'erreur de l'application*/
             .LoginError{
@@ -226,7 +306,6 @@ class CoreXLogin {
                 padding: 2vh;
                 border: solid 0px #dcdcdc;
                 border-bottom: solid 1px #dcdcdc;
-                margin: 2% 0px 2% 0px;
             }
             .LoginInput:focus,
             .LoginInput.focus {
@@ -253,6 +332,24 @@ class CoreXLogin {
                 align-content:center;
                 align-items: center;
             }
+            .LoginBox{
+                margin: 0px auto 0px auto;
+                background-color: white; 
+                width:30%; 
+                text-align: left; 
+                box-shadow: 0 10px 20px rgba(0, 0, 0, .2); 
+                padding: 5vh;
+            }
+
+            .CoreXWindowUserConfigInput:focus,
+            .CoreXWindowUserConfigInput.focus {
+                border-color: var(--CoreX-color);
+            }
+            @media (hover: hover) {
+                .CoreXWindowUserConfigInput:hover{
+                    border-color: var(--CoreX-color);
+                }
+            }
 
             @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait),
             only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: portrait),
@@ -261,10 +358,11 @@ class CoreXLogin {
                 #LoginTitre{font-size: var(--CoreX-TitreIphone-font-size);}
                 .LoginText{font-size: var(--CoreX-Iphone-font-size);}
                 .LoginTextBig{font-size: calc(var(--CoreX-Iphone-font-size)*1.5);}
-                #LoginBox{width: 90%;}
                 .LoginInput {font-size: var(--CoreX-Iphone-font-size);}
                 .LoginError{font-size: var(--CoreX-Iphone-font-size);}
                 .LoginButton{font-size: var(--CoreX-Iphone-font-size); border-radius: 40px;}
+                .LoginBox{width:80%; padding: 2vh;}
+                .CoreXWindowUserConfigInput {padding: 3%;}
             }
 
             @media screen and (min-width: 1200px)
@@ -272,7 +370,6 @@ class CoreXLogin {
                 #LoginTitre{font-size: var(--CoreX-TitreMax-font-size);}
                 .LoginText{font-size: var(--CoreX-Max-font-size);}
                 .LoginTextBig{font-size: calc(var(--CoreX-Max-font-size)*1.5);}
-                #LoginBox{max-width: 500px;}
                 .LoginInput {font-size: var(--CoreX-Max-font-size);}
                 .LoginError{font-size: var(--CoreX-Max-font-size);}
                 .LoginButton{font-size: var(--CoreX-Max-font-size); border-radius: 40px;}
